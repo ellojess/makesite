410982cd4eeee2560e652541c18750e26595bd4b
Jess <jtrinh619@gmail.com>
2021-01-30 22:54:54 -0800

    implement read, write, and render to file

--- makesite.go
+++ makesite.go
@@ -3,42 +3,65 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"html/template"
+	"os"
 )
 
-type Data struct {
+type Content struct {
 	Content string
 }
 
-func readFile() {
-	// read file 
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+func readFile(file string) string{
+
+	fileContents, err := ioutil.ReadFile(file)
 	if err != nil {
-		// A common use of `panic` is to abort if a function returns an error
-		// value that we don’t know how to (or want to) handle. This example
-		// panics if we get an unexpected error when creating a new file.
 		panic(err)
 	}
-	fmt.Print(string(fileContents))
+	return string(fileContents)
 }
 
-func writeFile() {
-	// write file 
-	bytesToWrite := []byte("hello\ngo\n")
-	err := ioutil.WriteFile("first-post.txt", bytesToWrite, 0644)
+func writeFile(file string, data string) {
+	bytesToWrite := []byte(data)
+	err := ioutil.WriteFile(file, bytesToWrite, 0644)
+
 	if err != nil {
 		panic(err)
 	}	
 }
 
-func writeTemplateToFilesystem() {
-	
+//  Render contents of first-post.txt using Go Templates and print it to stdout
+func renderTemplate(filename string, data string) {
+	t := template.Must(template.New("template.tmpl").ParseFiles(filename))
+	content := Content{Content: data}
+	err := t.Execute(os.Stdout, content)
+
+	if err != nil {
+	  panic(err)
+	}
+}
+
+//  Write the HTML template to the filesystem to a file. Name it first-post.html.
+func writeTemplateToFile(filename string, data string) {
+	t := template.Must(template.New("template.tmpl").ParseFiles(filename))
+	content := Content{Content: data}
+	f, err := os.Create("first-post.html")
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.Execute(f, content)
+	if err != nil {
+		panic(err)
+	  }
+
 }
 
 
 func main() {
 	fmt.Println("Hello, world!")
 
-	readFile()
-	// writeFile()
+	// readFile()
+	renderTemplate("template.tmpl", readFile("first-post.txt"))
+	writeTemplateToFile("template.tmpl", readFile("first-post.txt"))
 
 }
